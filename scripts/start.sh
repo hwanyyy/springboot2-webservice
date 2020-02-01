200b00e7a8064347d54e9b46796610685d12bb20
hwanyyy <goooogler94@gmail.com>
2020-02-01 21:24:09 +0900

    [fix] 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,7 @@ PROJECT_NAME=springboo2-webservice
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 
-cp ${REPOSITORY}/zip/*.jar ${REPOSITORY}/
+cp ${REPOSITORY}/zip/build/libs/*.jar ${REPOSITORY}/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr ${REPOSITORY}/zip/build/*.jar | tail -n 1)
