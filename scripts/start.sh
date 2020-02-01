cc48cbce57264d8e569f37bee58b28b554d2314a
hwanyyy <goooogler94@gmail.com>
2020-02-01 21:19:32 +0900

    [fix] 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,7 +14,7 @@ echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 cp ${REPOSITORY}/zip/*.jar ${REPOSITORY}/
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr ${REPOSITORY}/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr ${REPOSITORY}/zip/build/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
